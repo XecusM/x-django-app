24a17279eca0b3b91ac275c28e483cc803fa3570
Mohamed Aboel-fotouh <abo.elfotouh@live.com>
2020-05-09 01:41:14 +0200

    add test files and forms file for xapp

--- demo/x_django_app/views.py
+++ demo/x_django_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import (
+                                TemplateView, ListView,
+                                DetailView, CreateView,
+                                UpdateView, DeleteView
+                            )
 # Create your views here.
+
+
+class XListView(ListView):
+    '''
+    List view with search and sort criteria
+    '''
