ed31e5df6799dca18e2de823880ec31fdf1ac895
Mohamed Aboel-fotouh <abo.elfotouh@live.com>
2020-05-09 07:49:05 +0200

    add search form and init views

--- demo/x_django_app/forms.py
+++ demo/x_django_app/forms.py
@@ -1,1 +1,13 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+class SearchForm(forms.Form):
+    '''
+    Form for search bar of the XListView
+    '''
+    search = forms.CharField(
+                            label=_('Search'),
+                            max_length=128,
+                            help_text=_('Enter your search keywords here')
+                            required=True)
--- demo/x_django_app/views.py
+++ demo/x_django_app/views.py
@@ -11,3 +11,61 @@ class XListView(ListView):
     '''
     List view with search and sort criteria
     '''
+    search_fields = list()
+    filter_fields = list()
+    model = object()
+    queryset = object()
+    order_by = list()
+    success_url = str()
+
+    def post(self, request, *args, **kwargs):
+        '''
+        add keywords to url in post search
+        '''
+        keys =list()
+        if request.POST['search']:
+            keys.append(f"search={request.POST['search']}")
+
+        return get_success_url
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        '''
+        Get the context for this view.
+        '''
+        context = super().get_context_data(**kwargs)
+
+        if self.request.GET.get('filter'):
+            filter = self.request.GET.get('filter')
+            kwargs.update({'filter': filter})
+        if self.request.GET.get('search'):
+            search = self.request.GET.get('search')
+            kwargs.update({'search': search})
+        if self.request.GET.get('sort'):
+            sort = self.request.GET.get('sort')
+            kwargs.update({'sort': sort})
+        context.update(kwargs)
+
+        return context
+
+    def get_queryset(self):
+        '''
+        get articles
+        '''
+        if self.queryset:
+            queryset = self.queryset
+        else:
+            queryset = self.model.objects.all()
+
+        if self.request.GET.get('search'):
+            search = self.request.GET.get('search')
+            queryset = queryset.filter(
+                            title__contains=search).order_by('-created_at')
+        if self.request.GET.get('filter'):
+            filter = self.request.GET.get('filter')
+            queryset = queryset.filter(
+                            category=filter).order_by('-created_at')
+        if self.request.GET.get('sort'):
+            sort = self.request.GET.get('sort')
+            queryset = queryset.all().order_by(sort)
+        
+        return queryset
