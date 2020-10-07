d4c782a31d71aa0bce3dc49c92b390550113c8fa
abo.elfotouh@live.com <abo.elfotouh@live.com>
2020-10-07 22:53:07 +0200

    add compiled tranlation

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name="x-django-app",
-    version="1.0.5",
+    version="1.0.6",
     description="A Python package to get weather reports for any location.",
     long_description=readme(),
     long_description_content_type="text/markdown",
