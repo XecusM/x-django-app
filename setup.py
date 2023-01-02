51605483e92f3d4fa7267860c894f1c78f515287
Mohamed Aboel-fotouh <abo.elfotouh@live.com>
2023-01-02 23:09:59 +0200

    fix language issue

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name="x-django-app",
-    version="1.2.1",
+    version="1.2.2",
     description="A Python package to get weather reports for any location.",
     long_description=readme(),
     long_description_content_type="text/markdown",
